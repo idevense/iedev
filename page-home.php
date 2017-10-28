6f5e01d624ca5eadc9582aad0e2d83f2b09dd85b
Unknown <idevense@gmail.com>
2017-10-28 23:04:01 +0200

    css grid layout established

---
+++ page-home.php
@@ -0,0 +1,29 @@
+<?php
+/** Template Name: homepage
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package iedev
+ */
+
+get_header(); ?>
+
+	
+		<main id="main" class="site-main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	
+
+<?php
+get_sidebar();
+get_footer();
